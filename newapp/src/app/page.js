185e17ddfdc683af9922a677412e0a1ca02ec92a
supipi99 <supipi@sutratechnologies.com>
2025-02-05 17:11:40 +0530

    move nextjs project

---
+++ newapp/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <> <h1>hello word eeeeeeS </h1>
+    <h2>yoooooo!!!!!</h2>
+    </>
+   
+  );
+}
